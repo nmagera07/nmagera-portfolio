27180af9a93282d91c2c8d53985105a611c006d5
Nate Magera <nmagera07@gmail.com>
2019-10-16 15:30:15 -0400

    changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const NavBar = (props) => {
     return ( 
         <div className="navbar">
-            {/* <h1>Nate Magera</h1> */}
+            
        
         </div>
      );
